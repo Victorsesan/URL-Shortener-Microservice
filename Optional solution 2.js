c820c316f0e1a5c09d12e656bb5c7ade603e3040
Victorsesan <156327324+Victorsesan@users.noreply.github.com>
2024-07-06 02:56:41 +0100

    Update Optional solution 2.js

--- Optional solution 2.js
+++ Optional solution 2.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Import mongoose module
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
